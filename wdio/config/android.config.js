0cbcb256bae229fdc002ebcc11b6195c4efffc59
Curtis <Curtis.David7@gmail.com>
2022-08-02 21:13:03 -0400

    first attempt at appium

--- wdio/config/android.config.js
+++ wdio/config/android.config.js
@@ -2,20 +2,20 @@ const { config } = require('../wdio.conf');
 
 // Appium capabilities
 config.capabilities = [
-	{
-		platformName: 'Android',
-		noReset: true,
-		fullReset: false,
-		maxInstances: 1,
-		deviceName: 'Pixel 3 API 29',
-		platformVersion: '10',
-		app: '../android/app/build/outputs/apk/debug/app-debug.apk',
-		appPackage: 'io.metamask',
-		appActivity: '.MainActivity',
-		automationName: 'UiAutomator2',
-	},
+  {
+    platformName: 'Android',
+    noReset: true,
+    fullReset: false,
+    maxInstances: 1,
+    deviceName: 'Pixel_4_API_30',
+    platformVersion: '11',
+    app: '../../android/app/build/outputs/apk/debug/app-debug.apk',
+    appPackage: 'io.metamask',
+    appActivity: '.MainActivity',
+    automationName: 'UiAutomator2',
+  },
 ];
 
-config.cucumberOpts.tagExpression = '@androidApp'; // pass tag to run tests specific to android
+//config.cucumberOpts.tagExpression = '@androidApp'; // pass tag to run tests specific to android
 
 exports.config = config;
